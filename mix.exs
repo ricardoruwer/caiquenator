1dab9e60b9dea27598a636321e8519a732942d79
Marcos Ferreira <marcosp4f@gmail.com>
2018-11-14 11:37:09 -0200

    Upgrade dependencies to phoenix 1.4, use Jason as json library

--- mix.exs
+++ mix.exs
@@ -32,14 +32,16 @@ defmodule Caiquenator.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
-      {:phoenix_pubsub, "~> 1.0"},
+      {:floki, "~> 0.20"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:yaml_elixir, "~> 2.1"},
-      {:floki, "~> 0.20"}
+      {:phoenix_pubsub, "~> 1.0"},
+      {:plug, "~> 1.7"},
+      {:plug_cowboy, "~> 2.0"},
+      {:yaml_elixir, "~> 2.1"}
     ]
   end
 end
